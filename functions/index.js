e716d9b3b7fb519b9e16ffba9167141fdd6ff8ff
Arturo Rios <arturo_rios@epam.com>
2022-05-26 16:22:21 -0500

    functions added

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+// var Dog =  function () {
+//     var name, breed
+// }
+
+// var speak = function (say){
+//     console.log(say)
+// }
+
+// Dog.prototype.speak = speak
+
+// firstDog = new Dog
+// firstDog.name = 'Rover'
+// firstDog.breed = 'doberman'
+// firstDog.speak('woof')
+
+
+// var speak = function (what){
+//     console.log(what)
+//     console.log(this.love)
+//     // console.log(this)
+// }
+
+// var saySomething = {normal: 'mew', love: 'purr'}
+// speak.call(saySomething,'testing')
+
+// var plus = function(){
+//     console.log(arguments[0])
+// }
+
+// plus(2,3,1)
+
+let test  = function(){
+    console.log('its working')
+}()
