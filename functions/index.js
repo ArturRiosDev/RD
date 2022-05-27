057d4d6b80fa174bf5891baf4128a2b7c6c3e49e
Arturo Rios <arturo_rios@epam.com>
2022-05-27 09:18:21 -0500

    did some examples about this

--- functions/index.js
+++ functions/index.js
@@ -29,6 +29,35 @@
 
 // plus(2,3,1)
 
-let test  = function(){
-    console.log('its working')
-}()
+// let test  = function(){
+//     console.log('its working')
+// }()
+
+// let dog = function (){
+//     let name, breed
+// }
+
+// firstDog = new dog
+// firstDog.name = 'luka'
+// firstDog.breed = 'poodle'
+
+// console.log(firstDog)
+
+// let jump = function(meters){
+//     firstDog.color = 'white'
+//     console.log(`This dog can jump ${meters} high`)
+// }
+
+// dog.prototype.jump = jump
+// firstDog.jump(5)
+// console.log(firstDog)
+
+var speak = function (what){
+        console.log(what)
+        console.log(this.normal)
+        // console.log(this)
+    }
+    
+    var saySomething = {normal: 'mew', love: 'purr'}
+    speak.call(saySomething,'testing')
+    
